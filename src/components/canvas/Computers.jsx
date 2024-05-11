f93d9b66bdfbb463b829d89656a4b40743bf4af9
StevenJohnss <62972966+StevenJohnss@users.noreply.github.com>
2024-05-11 17:48:55 +0300

    'update'

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -55,13 +55,13 @@ const ComputersCanvas = () => {
 
   return (
     <Canvas
-      frameloop='demand'
+      frameloop="demand"
       shadows
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
-      <Suspense fallback={<CanvasLoader />}>
+      <Suspense>
         <OrbitControls
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
