9ddc7e90518991eb837c4a2b5571e7763fa17c84
StevenJohnss <62972966+StevenJohnss@users.noreply.github.com>
2024-12-31 15:07:50 +0200

    'update'

--- src/components/PaymobPayment.jsx
+++ src/components/PaymobPayment.jsx
@@ -14,7 +14,7 @@ const PaymobPayment = ({ amount, integrationID, children }) => {
       const host_url = process.env.NODE_ENV === 'development' 
         ? process.env.REACT_APP_BE_LOCAL_URL 
         : process.env.REACT_APP_BE_PROD_URL;
-      const request = await fetch(`${host_url}/api/paymob/create-payment`, {
+      const request = await fetch(`https://portfolio-web-app-fawn.vercel.app/api/paymob/create-payment`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
