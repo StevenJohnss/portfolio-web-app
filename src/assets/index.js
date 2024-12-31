d51ff9a1fdf85e421388f08d276bafb6bceb1804
StevenJohnss <62972966+StevenJohnss@users.noreply.github.com>
2024-12-31 22:06:01 +0200

    'update'

--- src/assets/index.js
+++ src/assets/index.js
@@ -28,10 +28,10 @@ import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
 
-import insite from "./company/4insite_logo.jfif";
+import insite from "./company/fouinsite.jfif";
 import corecxe from "./company/corecxe.jfif";
 import instatus from "./company/instatus.jfif";
-import bue from "./company/the_british_university_in_egypt_logo.jfif";
+import bue from "./company/bue.jfif";
 
 import ecommerce from "./Article_Thumbnail_Ecommerce_Website.jpg";
 import prizewheel from "./spin-to-win-wheel-gamification.jpg";
